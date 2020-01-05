3812d2265267066c3533a920c34f3070e8f55f0a
SamSaltyScience <sadonohoo@gmail.com>
2020-01-04 19:25:52 -0800

    Commit Take #whatever

---
+++ Practice.R
@@ -0,0 +1,13 @@
+print("This file was created within RStudio")
+
+print("And now it lives on GitHub")
+
+library("ggplot2")
+
+git config -- global user.email "Sadonohoo@gmail.com"
+
+git config --global
+
+man git-config
+
+git config user.email "Sadonohoo@gmail.com"
